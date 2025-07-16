100f77872b5c52658bc406ca6287bfae3aad8c60
AnisShameem <anisoshameem@outlook.com>
2025-07-16 07:01:30 +0600

    2nd

--- contact_manager.php
+++ contact_manager.php
@@ -3,10 +3,21 @@
 
 $contacts = [];
 
-function addContact(array &$contacts, string $name, string $email, int$phone){
-    
+function addContact(array &$contacts, string $name, string $email, int $phone):void
+{
+    $contacts[] = ['name' => $name, 'email' => $email, 'phone' => $phone];
+    //array_push($contacts[] = ['name' => $name, 'email'=> $email, 'phone' => $phone]);
 }
-
+ function displaycontact(array $contacts):void 
+ {
+    if(empty($contacts)){
+        echo "N contacts Available.\n";
+    } else{
+        foreach($contacts as $contact){
+            
+        }
+    }
+ }
 
 
 ?>
