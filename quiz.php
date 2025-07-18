98ad8a920ad6756ca91bb8205866bddfb392b5c0
AnisShameem <anisoshameem@outlook.com>
2025-07-18 10:53:47 +0600

    2nd

--- quiz.php
+++ quiz.php
@@ -12,10 +12,29 @@
     echo ($index + 1). "." .$question['questions']. \n;
     $answers[] = trim(readline("Your Answer : "));
     
-
   }
 
+  function evaluateQuiz(array $questions, array $answers):string
+  {
+    $score = 0;
+    foreach($questions as $index => $question){
+        if($answers[$index] === $questions['correct']){
+            $score++;
+        }
+    }
+    return $score ;
+}
 
+$myScore = evaluateQuiz($questions , $answers);
 
+echo "\n You scored $myScore out of ". count($questions). ".\n";
+
+if($myScore === count($questions)){
+    echo "Excellent Job!\n";
+}elseif($myScore >= 1){
+    Echo "Good job! \n";
+}else{
+    echo "You failed. Try again";
+}
 
 ?>
