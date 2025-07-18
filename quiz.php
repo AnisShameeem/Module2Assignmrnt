fb8f7bb4e917d767b63ed94489bf18b7091e5eea
AnisShameem <anisoshameem@outlook.com>
2025-07-18 10:30:41 +0600

    first

--- quiz.php
+++ quiz.php
@@ -1,5 +1,21 @@
-<? php
-  //checking github connections
+<?php
+  $questions = [
+    ['questions' => 'What is 2+2? ' , 'correct'  => '4'],
+    ['questions' => 'Capital of bd? ' , 'correct'  => 'Dhaka'],
+    ['questions' => 'Who wrote agnibina? ' , 'correct'  => 'Nazrul'],
+  ];
+
+  $answers = [];
+
+  foreach($questions as $index => $question)
+  {
+    echo ($index + 1). "." .$question['questions']. \n;
+    $answers[] = trim(readline("Your Answer : "));
+    
+
+  }
+
+
 
 
 ?>
